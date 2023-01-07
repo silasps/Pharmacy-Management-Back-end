5c225b9034e3a58d90291ed37bcff522c6572fc2
silasps <silaspereiras@gmail.com>
2023-01-07 16:44:29 -0300

    Repositories creation

--- src/main/java/com/pharmacy/repository/AddressRepository.java
+++ src/main/java/com/pharmacy/repository/AddressRepository.java
@@ -1,6 +1,8 @@
 package com.pharmacy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository {
 }
